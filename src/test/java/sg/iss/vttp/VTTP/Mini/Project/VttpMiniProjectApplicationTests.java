3502fe04bfd345ba9a939467593527877579b8d6
Your Name <you@example.com>
2022-05-25 05:44:16 +0800

    try tests

--- src/test/java/sg/iss/vttp/VTTP/Mini/Project/VttpMiniProjectApplicationTests.java
+++ src/test/java/sg/iss/vttp/VTTP/Mini/Project/VttpMiniProjectApplicationTests.java
@@ -1,9 +1,17 @@
 package sg.iss.vttp.VTTP.Mini.Project;
 
+import java.net.http.HttpHeaders;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import sg.iss.vttp.VTTP.Mini.Project.Models.Restaurant;
 import sg.iss.vttp.VTTP.Mini.Project.Repositories.AppUserRepository;
 import sg.iss.vttp.VTTP.Mini.Project.Repositories.ApplicationRepository;
 import sg.iss.vttp.VTTP.Mini.Project.Services.AppUserService;
@@ -11,6 +19,7 @@ import sg.iss.vttp.VTTP.Mini.Project.Services.ApplicationService;
 import sg.iss.vttp.VTTP.Mini.Project.Services.LocationService;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class VttpMiniProjectApplicationTests {
 
 	@Autowired
@@ -22,9 +31,20 @@ class VttpMiniProjectApplicationTests {
 	@Autowired
 	LocationService locationService;
 
+	@Autowired
+	MockMvc mav;
+	
 	@Test
-	void contextLoads() {
+	void shouldRedirect() throws Exception {
+		RequestBuilder initialRedirectReq = MockMvcRequestBuilders.get("/");
+		mav.perform(initialRedirectReq);
 		
-	}
+		RequestBuilder loginFormReq = MockMvcRequestBuilders.get("/login");
+		mav.perform(loginFormReq).andExpect(MockMvcResultMatchers.view().name("login"));
 
+		RequestBuilder loginReq = MockMvcRequestBuilders.post("/login")
+		.param("username", "World")
+		.param("password","Hello");
+		mav.perform(loginReq).andExpect(MockMvcResultMatchers.view().name("http://localhost/processing"));
+	}
 }
