79028641d510d2df25959c6aacaec873f36c2af1
Your Name <you@example.com>
2022-05-25 06:26:31 +0800

    Service and Repo Testing

--- src/test/java/sg/iss/vttp/VTTP/Mini/Project/VttpMiniProjectApplicationTests.java
+++ src/test/java/sg/iss/vttp/VTTP/Mini/Project/VttpMiniProjectApplicationTests.java
@@ -1,16 +1,22 @@
 package sg.iss.vttp.VTTP.Mini.Project;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrowsExactly;
+
 import java.net.http.HttpHeaders;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.util.Assert;
 
+import sg.iss.vttp.VTTP.Mini.Project.Models.Reservation;
 import sg.iss.vttp.VTTP.Mini.Project.Models.Restaurant;
 import sg.iss.vttp.VTTP.Mini.Project.Repositories.AppUserRepository;
 import sg.iss.vttp.VTTP.Mini.Project.Repositories.ApplicationRepository;
@@ -35,9 +41,9 @@ class VttpMiniProjectApplicationTests {
 	MockMvc mav;
 	
 	@Test
-	void shouldRedirect() throws Exception {
+	void loginSequence() throws Exception {
 		RequestBuilder initialRedirectReq = MockMvcRequestBuilders.get("/");
-		mav.perform(initialRedirectReq);
+		mav.perform(initialRedirectReq).andExpect(MockMvcResultMatchers.redirectedUrl("http://localhost/login"));
 		
 		RequestBuilder loginFormReq = MockMvcRequestBuilders.get("/login");
 		mav.perform(loginFormReq).andExpect(MockMvcResultMatchers.view().name("login"));
@@ -45,6 +51,39 @@ class VttpMiniProjectApplicationTests {
 		RequestBuilder loginReq = MockMvcRequestBuilders.post("/login")
 		.param("username", "World")
 		.param("password","Hello");
-		mav.perform(loginReq).andExpect(MockMvcResultMatchers.view().name("http://localhost/processing"));
+		mav.perform(loginReq).andExpect(MockMvcResultMatchers.redirectedUrl("/processing"));
 	}
+
+	@Test
+	void failLogin() throws Exception {
+		RequestBuilder loginReq = MockMvcRequestBuilders.post("/login")
+		.param("username", "Fail")
+		.param("password","Login");
+		mav.perform(loginReq);
+	}
+
+	@Test
+	void googeEmbed() throws Exception {
+
+		Assert.notNull(locationService.getGoogleMapsEmbedLink("Tampines+Mall"), "Not Empty");
+		
+	}
+
+	@Test
+	void applicationServiceTests() throws Exception {
+		Reservation sample = new Reservation();
+		sample.setCustomerName("Hello");
+		sample.setCustomerPax(4);
+		sample.setReservationDate("2022-11-22");
+		sample.setReservationId("FAKEUUID");
+		sample.setReservationStatus(false);
+		sample.setReservationTime("0830");
+		sample.setRestaurantName("Pandas");
+		appService.createReservation(sample);
+
+		appService.getReservationsByCustName("Hello");
+
+		appService.deleteReservationWithId("FAKEUUID");
+	}
+
 }
